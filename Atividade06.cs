bf8822416a7a05c533b6566bc6d63749fb624bbb
oppestana <pedro.t.pestana@gmail.com>
2026-05-28 14:23:59 -0300

    Ordenar numeros na Matriz

---
+++ Atividade06.cs
@@ -0,0 +1,34 @@
+
+/*
+
+
+int[,] numeros =
+{
+    {1, 2, 3},
+    {4, 5, 6}
+};
+
+int vSup = numeros[0,0];
+
+for(int linha = 0; linha < 2; linha++)
+{
+    for(int coluna = 0; coluna < 3; coluna++)
+    {
+        if(numeros[linha, coluna] > vSup)
+        {
+            vSup = numeros[linha,coluna];
+            
+        }
+    }
+}
+Console.WriteLine($"O maior valor da matriz é: {vSup}");
+
+
+
+
+
+
+
+
+
+*/
