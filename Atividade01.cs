1b8017da5b2caea4cf0c1e7792744403c361d0d1
oppestana <pedro.t.pestana@gmail.com>
2026-05-28 14:21:31 -0300

    Nomes na tabela

---
+++ Atividade01.cs
@@ -0,0 +1,27 @@
+
+
+/*
+Console.Clear();
+string[,] nomes = new string [2, 2];
+
+for (int linha = 0; linha < 2; linha++)
+{
+    for (int coluna = 0; coluna < 2; coluna++)
+    {
+        Console.Write($"Digite o nome: ");
+        nomes[linha, coluna] = Console.ReadLine()!;
+    }
+
+}
+Console.WriteLine();
+
+for (int linha = 0; linha < 2; linha++)
+{
+    for (int coluna = 0; coluna < 2; coluna++)
+    {
+        Console.Write(nomes[linha, coluna] + "\t");
+    }
+    Console.WriteLine();
+}
+
+*/
