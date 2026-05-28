3e76aaa135f23a4d0289d792545ad7f5bd25e68e
oppestana <pedro.t.pestana@gmail.com>
2026-05-28 14:24:18 -0300

    Atividade Matriz

---
+++ Desafio.cs
@@ -0,0 +1,50 @@
+
+
+
+/*
+
+int[,] numeros = new int[3, 3];
+int resposta;
+
+
+for(int linha = 0; linha < 3; linha++)
+{
+    for (int coluna = 0; coluna < 3; coluna++)
+    {
+        
+        for (int i = 0; i < 3; i++)
+        {
+        Console.Write($"Digite o {i + 1}º numero: ");
+        resposta = int.Parse(Console.ReadLine()!);
+            if (resposta > 0 || resposta < 10)
+            {
+                numeros[linha,coluna] = resposta;
+            }
+            else
+            {
+                i--;
+            }
+        }
+    }
+}
+
+for(int linha = 0; linha < 2; linha++)
+{
+    for(int coluna = 0; coluna < 3; coluna++)
+    {
+        Console.Write(numeros[linha, coluna] + "\t");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
+
+
+*/
+
+
+
+
