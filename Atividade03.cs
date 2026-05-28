bc53d1b8fca24f5ecb38f68869a11a084ac7ca2d
oppestana <pedro.t.pestana@gmail.com>
2026-05-28 14:22:22 -0300

    Soma de numeros na Matriz

---
+++ Atividade03.cs
@@ -0,0 +1,23 @@
+
+
+
+
+/*
+Console.Clear();
+int[,] numeros =
+{
+    { 10, 20},
+    { 15, 25}
+};
+
+double soma = 0;
+
+for(int linha = 0; linha < 2; linha++)
+{
+    for(int coluna = 0; coluna < 2; coluna++)
+    {
+        soma += numeros[linha, coluna];
+    }
+}
+Console.WriteLine(soma);
+*/
