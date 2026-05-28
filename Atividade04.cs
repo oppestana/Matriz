afdaa5b1b10d50a0a31b68aa5ed081e95450906a
oppestana <pedro.t.pestana@gmail.com>
2026-05-28 14:23:07 -0300

    Definir media na Matriz

---
+++ Atividade04.cs
@@ -0,0 +1,29 @@
+
+
+
+/*
+
+Console.Clear();
+double[,] numeros =
+{
+    {9, 8},
+    {6.5, 7},
+    {8, 10}
+};
+
+double soma = 0;
+int divisor = 0;
+double media = 0;
+
+for (int linha = 0; linha < 3; linha++)
+{
+    for(int coluna = 0; coluna < 2; coluna++)
+    {
+        soma += numeros[linha, coluna];
+        divisor++;
+    }
+    media = soma / divisor;
+}
+Console.Write($"A media geral do aluno é: {media:F2}");
+
+*/
