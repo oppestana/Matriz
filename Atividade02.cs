598c1480efdb976a23538112d07047e7ae596d6e
oppestana <pedro.t.pestana@gmail.com>
2026-05-28 14:21:54 -0300

    Numeros na tabela

---
+++ Atividade02.cs
@@ -0,0 +1,26 @@
+/*
+
+Console.Clear();
+int[,] numeros = new int [2, 3];
+
+for(int linha = 0; linha < 2; linha++)
+{
+    for(int coluna = 0; coluna < 3; coluna++)
+    {
+        Console.Write("Digite os numeros: ");
+        numeros[linha, coluna] = int.Parse(Console.ReadLine()!);
+    }
+}
+
+Console.WriteLine();
+
+for(int linha = 0; linha < 2; linha++)
+{
+    for(int coluna = 0; coluna < 3; coluna++)
+    {
+        Console.Write(numeros[linha, coluna] + "\t");
+    }
+    Console.WriteLine();
+}
+
+*/
